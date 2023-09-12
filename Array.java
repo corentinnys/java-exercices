57dcd2821f99143dd3422d2bdeb4f3ec83f9c318
corentin nys <corentinys@gmail.com>
2023-09-12 09:53:34 +0200

    exercice array

---
+++ Array.java
@@ -0,0 +1,15 @@
+public class Array {
+    public static void main(String[] args)
+    {
+        int[] array  = {5,12,13,4,4,7,58,60,1,3};
+
+        for (int i=0;i<array.length;i++)
+        {
+            if(array[i]<10){
+                System.out.println(array[i]);
+            }
+        }
+    }
+
+
+}
